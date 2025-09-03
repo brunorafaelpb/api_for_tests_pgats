abca3b7fe91b1bcf0c0110b89de275f630fccb75
brad <brad@cesar.org.br>
2025-09-03 13:06:52 -0300

    Implementaçao do teste para sucesso 201

--- test/controller/transferController.test.js
+++ test/controller/transferController.test.js
@@ -23,7 +23,7 @@ describe('Tranfer Controller', () => {
         it('USANDO MOCKS: Quando informo remetente e destinatário inexistentes recebo 400', async () => {
             // Mocar apenas a função transfer do Service
             const transferServiceMock = sinon.stub(transferService, 'createTransfer')
-            transferServiceMock.throws(new Error('Usuário remetente ou destinatário não  encontrado'));
+            transferServiceMock.throws(new Error('Usuário remetente ou destinatário não encontrado'));
 
             const response = await request(app)
                 .post('/transfer')
@@ -34,6 +34,30 @@ describe('Tranfer Controller', () => {
                 });
             expect(response.status).to.equal(400);
             expect(response.body).to.have.property('error', 'Usuário remetente ou destinatário não encontrado');
+
+            sinon.restore();
+        });
+
+        it('USANDO MOCKS: Quando informo valores válidos eu tenho sucesso com 201 CREATED', async () => {
+            // Mocar apenas a função transfer do Service
+            const transferServiceMock = sinon.stub(transferService, 'createTransfer')
+            transferServiceMock.returns({
+                from: 'bruno',
+                to: 'fernanda',
+                amount: 1000
+            });
+
+            const response = await request(app)
+                .post('/transfer')
+                .send({ 
+                    from: "bruno",
+                    to: "fernanda",
+                    amount: 1000
+                });
+            expect(response.status).to.equal(201);
+            expect(response.body).to.have.property('from', 'bruno');
+            expect(response.body).to.have.property('to', 'fernanda');
+            expect(response.body).to.have.property('amount', 1000);
 
             sinon.restore();
         });
